80497693c603f6c98184454cc1fa23781e51fbed
vin146 <e232737@edu.sbl.ch>
2026-03-15 10:36:09 +0100

    Video wechselbar mit Knopf

---
+++ script.js
@@ -0,0 +1,16 @@
+let aktuellesVideo = 1;
+
+function wechselVideo() {
+
+    let video = document.getElementById("sideVideo");
+
+    if (aktuellesVideo === 1) {
+        video.src = "76041-557904747_small.mp4";
+        aktuellesVideo = 2;
+    } else {
+        video.src = "1456996-hd_1920_1080_30fps.mp4";
+        aktuellesVideo = 1;
+    }
+
+    video.play();
+}
