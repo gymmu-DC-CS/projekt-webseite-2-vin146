6a0d5a1f862e14f5dbe89a976d111e0b5f383c1d
vin146 <e232737@edu.sbl.ch>
2026-03-15 10:52:54 +0100

    Drittes Video zum wechseln + Knopffarbe geändert

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ function wechselVideo() {
     let video = document.getElementById("sideVideo");
 
     if (aktuellesVideo === 1) {
-        video.src = "76041-557904747_small.mp4";
+        video.src = "76041-557904747_small.mp4"; 
         aktuellesVideo = 2;
-    } else {
-        video.src = "1456996-hd_1920_1080_30fps.mp4";
+    } 
+    else if (aktuellesVideo === 2) {
+        video.src = "16612175-hd_1920_1080_30fps.mp4";    
+        aktuellesVideo = 3;
+    } 
+    else {
+        video.src = "1456996-hd_1920_1080_30fps.mp4"; 
         aktuellesVideo = 1;
     }
 
